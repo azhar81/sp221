9834f18727b2950a0053f30b605f3fe67054240d
Muhammad Azhar Hassanuddin <a.hassanuddin02@gmail.com>
2022-02-28 00:03:49 +0700

    week 2 assignments

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
-# Mon 07 Feb 2022 23:02:21 WIB
+# Copyright (C) 2020-2022 Cicak Bin Kadal
 
-FILES="my*.txt my*.sh my*.tar.bz2.asc"
+WEEK="02"
+
+# This free document is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# REV06 Mon 21 Feb 2022 17:14:09 WIB
+# REV05 Wed 16 Feb 2022 14:40:24 WIB
+# REV04 Sat 20 Nov 2021 19:10:06 WIB
+# REV03 Mon 15 Nov 2021 11:22:19 WIB
+# REV02 Sun 19 Sep 2021 15:44:11 WIB
+# START Mon 28 Sep 2020 21:05:04 WIB
+
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key"
+
+REC2="170C70B0E9CF86FD"
+REC1="63FB12B215403B20"
+FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
-FILENAME="myW01.tar.bz2"
+RESDIR="$HOME/SP_RESULT/"
 
-echo "rm -f $FILENAME"
-rm -f $FILENAME
+[ -d $RESDIR ] || mkdir -p $RESDIR
+pushd $RESDIR
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -vf $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
 
-echo "rm -f $FILENAME.asc"
-rm -f $FILENAME.asc
-
-echo "tar cfj $FILENAME $HOME/SP_RESULT/W01"
-tar cfj $FILENAME $HOME/SP_RESULT/W01
-
-echo "gpg -o $FILENAME.asc -a -v -r MAH -r rms46@ui.ac.id -e $FILENAME"
-gpg -o $FILENAME.asc -a -v -r MAH -r rms46@ui.ac.id -e $FILENAME
-
-echo "rm -f $FILENAME"
-rm -f $FILENAME
+if [[ "$WEEK" != "00" ]] ; then
+    II="${RESDIR}myW$WEEK.tar.bz2.asc"
+    echo "Check and move $II..."
+    [ -f $II ] && mv -vf $II .
+fi
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
@@ -29,8 +53,16 @@ sha256sum $FILES > $SHA
 echo "sha256sum -c $SHA"
 sha256sum -c $SHA
 
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
+echo "gpg --output $SHA.asc --armor --sign --detach-sign $SHA"
+gpg --output $SHA.asc --armor --sign --detach-sign $SHA
 
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
+
+echo ""
+echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
+echo "==== ==== ==== ATTN: is this WEEK $WEEK ?? ==== ==== ==== ===="
+echo "==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ===="
+echo ""
+
+exit 0
